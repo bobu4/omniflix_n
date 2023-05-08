fdd5cc7e4637121c653871fdf225296979e202b8
bobu4 <48527047+bobu4@users.noreply.github.com>
2023-05-08 16:02:44 +0300

    Update omni.sh

--- omni.sh
+++ omni.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 val='omniflixvaloper1r7ay6ff5hudenmw0ryv0d3z2h5hc79zjakyt50'
 del='omniflix1r7ay6ff5hudenmw0ryv0d3z2h5hc79zjsyk248'
-pass='38bad1d12'
-wal_name='Darios[MantiCore]'
+pass=''
+wal_name='Darios'
 echo -e "${pass}\n" | omniflixhubd tx distribution withdraw-rewards ${val} --chain-id flixnet-3 --from ${wal_name} --commission --gas auto -y
 bal=$(omniflixhubd query bank balances ${del} -o json | jq -r '.balances  | .[].amount')
 echo -e "${pass}\n" | omniflixhubd tx staking delegate ${val} ${bal}uflix --chain-id flixnet-3 --from ${wal_name} --gas auto -y
